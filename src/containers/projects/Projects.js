e955aa29f7026f00109b21ed26fca4e47476cb17
rajnandini1900 <tanmayc2001@gmail.com>
2024-02-24 00:16:21 +0530

    Initial Commit

---
+++ src/containers/projects/Projects.js
@@ -0,0 +1,21 @@
+// import React, { useState } from "react";
+// import ProjectCard from "../../components/ProjectCard/";
+// import "./Project.css";
+
+// export default function Projects() {
+//   const [repo, _] = useState([]);
+
+//   return (
+//     <div className="main" id="opensource">
+//       <h1 className="project-title">Open Source Projects</h1>
+//       <div className="repo-cards-div-main">
+//         {repo.map((v, i) => {
+//           return <ProjectCard repo={v} key={"project-card-" + i} />;
+//         })}
+//       </div>
+//       <a className="" href="https://github.com/sanskritiiiiii">
+//         More Projects (Github)
+//       </a>
+//     </div>
+//   );
+// }
